04e42795ff7ad0770c7662cfb2326ef419960f4b
Aayush Kapoor <xeoneux@icloud.com>
2016-05-28 15:55:03 +0530

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,3 +5,20 @@ const path = require('path'),
     resolve = path.resolve;
 
 const getConfig = require('hjs-webpack');
+
+const root = resolve(__dirname);
+const src = join(root, 'src');
+const dest = join(root, 'dist');
+const modules = join(root, 'node_modules');
+
+const NODE_ENV = process.env.NODE_ENV;
+const isDev = NODE_ENV === 'development';
+
+var config = getConfig({
+    isDev: isDev,
+    in: join(src, 'app.js'),
+    out: dest,
+    clearBeforeBuild: true
+});
+
+module.exports = config;

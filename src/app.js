d82d636f4a9027ccc02d1bc288f0dee48afdc6a4
Aayush Kapoor <xeoneux@icloud.com>
2016-06-07 20:28:11 +0530

    Combine all routes

--- src/app.js
+++ src/app.js
@@ -6,18 +6,8 @@ import './app.css'
 import 'font-awesome/css/font-awesome.css'
 
 import App from 'containers/App/App'
+import makeRoutes from './routes'
 
-const Home = React.createClass({
-    render: function () {
-        return (<div>Hello world</div>)
-    }
-});
-
-const routes = (
-    <Router>
-        <Route path="/" component={Home}/>
-    </Router>
-);
-
+const routes = makeRoutes();
 const mountNode = document.querySelector('#root');
-ReactDOM.render(<App history={browserHistory}/>, mountNode);
+ReactDOM.render(<App history={browserHistory} routes={routes}/>, mountNode);
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {browserHistory, Router, Route, Redirect} from 'react-router'
+
+const Home = React.createClass({
+    render: function () {
+        return (<div>Hello world</div>)
+    }
+});
+
+export const makeRoutes = () => (
+    <Router>
+        <Route path="/" component={Home}/>
+        <Redirect from="*" to="/"/>
+    </Router>
+);
+
+export default makeRoutes;

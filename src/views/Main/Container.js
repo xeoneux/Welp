121227d530ca8f517e61162593c702a47efa216a
Aayush Kapoor <xeoneux@icloud.com>
2016-06-08 20:30:59 +0530

    Load nearby data for maps

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,14 +1,53 @@
 import React from 'react'
+import {searchNearby} from 'utils/googleApiHelpers'
 import Map, {GoogleApiWrapper} from 'google-maps-react'
 
 export class Container extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            places: [],
+            pagination: null
+        };
+    }
+
+    onReady(mapProps, map) {
+        const {google} = this.props;
+        const opts = {
+            location: map.center,
+            radius: '500',
+            types: ['cafe']
+        };
+
+        searchNearby(google, map, opts)
+            .then((results, pagination) => {
+                this.setState({
+                    places: results,
+                    pagination
+                });
+            })
+            .catch((status, result) => {
+
+            });
+    }
+
     render() {
         return (
             <div>
                 Hello from the container
                 <Map
                     onReady={this.onReady.bind(this)}
-                    google={this.props.google}/>
+                    google={this.props.google}
+                    visible={false}>
+
+                    {
+                        this.state.places.map(place => {
+                            return (<div key={place.id}>{place.name}</div>)
+                        })
+                    }
+
+                </Map>
             </div>
         )
     }

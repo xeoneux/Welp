526cd57fc8ec6eaea7a2f62429c81675f8e6f818
Aayush Kapoor <xeoneux@icloud.com>
2016-06-08 20:59:33 +0530

    Add listing component

---
+++ src/components/Listing/Listing.js
@@ -0,0 +1,25 @@
+import React, {PropTypes as T} from 'react'
+import classnames from 'classnames'
+
+import Item from './Item'
+import styles from './styles.module.css'
+
+export class Listing extends React.Component {
+    render() {
+        return (
+            <div className={classnames(styles.container)}>
+                {
+                    this.props.places.map(place => {
+                        return (
+                            <Item place={place}
+                                  onClick={this.props.onClick}
+                                  key={place.id}/>
+                        );
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default Listing;

1a51ddf614ed02b15dfe24ee1c9620417ccbddca
Aayush Kapoor <xeoneux@icloud.com>
2016-06-08 20:27:24 +0530

    Add Google API helper

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import Map, {GoogleApiWrapper} from 'google-maps-react'
 
 export class Container extends React.Component {
     render() {
         return (
             <div>
                 Hello from the container
+                <Map
+                    onReady={this.onReady.bind(this)}
+                    google={this.props.google}/>
             </div>
         )
     }
 }
 
-export default Container;
+export default GoogleApiWrapper({
+    apiKey: __GAPI_KEY__
+})(Container)

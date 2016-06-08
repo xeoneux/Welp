7d7772421b6badeead7ce67200cdf231e9e09885
Aayush Kapoor <xeoneux@icloud.com>
2016-06-08 21:04:00 +0530

    Add item component

---
+++ src/components/Listing/Item.js
@@ -0,0 +1,20 @@
+import React, {PropTypes as T} from 'react'
+import classnames from 'classnames'
+
+import Rating from 'components/Rating/Rating'
+import styles from './styles.module.css'
+
+export class Item extends React.Component {
+    render() {
+        const {place} = this.props;
+        return (
+            <div
+                className={styles.item}>
+                <h1 className={classnames(styles.title)}>{place.name}</h1>
+                <span>{place.rating / 5}</span>
+            </div>
+        );
+    }
+}
+
+export default Item;

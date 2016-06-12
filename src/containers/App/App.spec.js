276fd577461574572f5de41f5a38e7e7b0b477a5
Aayush Kapoor <xeoneux@icloud.com>
2016-06-12 23:45:49 +0530

    Update app container tests

--- src/containers/App/App.spec.js
+++ src/containers/App/App.spec.js
@@ -5,13 +5,20 @@ import {shallow} from 'enzyme'
 import App from './App'
 import styles from './styles.module.css'
 
-describe('<App />', function () {
+describe('<App />', () => {
     let wrapper;
+    let history = {};
     beforeEach(() => {
-        wrapper = shallow(<App />);
+        wrapper = shallow(<App history={history}/>);
     });
+
     it('has a Router component', () => {
         expect(wrapper.find('Router'))
             .to.have.length(1);
+    });
+
+    it('passes a history prop', () => {
+        const props = wrapper.find('Router').props();
+        expect(props.history).to.be.defined;
     });
 });

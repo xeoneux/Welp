6fa5d31c4ad0eba8e91573f60ce5f7657017a94a
Aayush Kapoor <xeoneux@icloud.com>
2016-06-12 23:55:40 +0530

    Fix and update map view

--- src/views/Main/Map/Map.js
+++ src/views/Main/Map/Map.js
@@ -10,21 +10,57 @@ export class MapComponent extends React.Component {
             return null;
         }
         return this.props.places.map(place => {
-            return <Marker key={place.id}
-                           name={place.id}
-                           position={place.geometry.location}
-                           onClick={this.props.onMarkerClick.bind(this)}
-            />
+            return <Marker
+                key={place.id}
+                name={place.id}
+                place={place}
+                label={place.name}
+                onClick={this.props.onMarkerClick.bind(this)}
+                map={this.props.map}
+                position={place.geometry.location}/>
         });
     }
 
+    renderChildren() {
+        const {children} = this.props;
+
+        if (React.Children.count(children) > 0) {
+            return React.Children.map(children, c => {
+                return React.cloneElement(c, this.props, {
+                    map: this.props.map,
+                    google: this.props.google
+                })
+            })
+        } else {
+            return this.renderMarkers();
+        }
+    }
+
     render() {
+        const {children} = this.props;
+
         return (
-            <Map google={this.props.google}
-                 className={styles.map}>
+            <Map map={this.props.map}
+                 google={this.props.google}
+                 className={styles.map}
+                 zoom={this.props.zoom}
+                 onRecenter={this.props.onMove}
+                 onDragend={this.props.onMove}
+                 onClick={this.props.onClick}
+                 visible={!children || React.Children.count(children) == 0}>
+                {this._renderChildren()}
             </Map>
         );
     }
 }
+
+MapComponent.propTypes = {
+    onMarkerClick: T.func
+};
+
+const identity = (...a) => a;
+MapComponent.defaultProps = {
+    onMarkerClick: identity
+};
 
 export default MapComponent;

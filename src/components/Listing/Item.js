adcfad12a54c58a50e32aad1df82c2b23fc9363b
Aayush Kapoor <xeoneux@icloud.com>
2016-06-12 23:38:31 +0530

    Update item component with props

--- src/components/Listing/Item.js
+++ src/components/Listing/Item.js
@@ -5,16 +5,45 @@ import Rating from 'components/Rating/Rating'
 import styles from './styles.module.css'
 
 export class Item extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hovered: false
+        }
+    }
+
+    onClick(e) {
+        this.props.onClick(this.props.place);
+    }
+
     render() {
         const {place} = this.props;
         return (
             <div
-                className={styles.item}>
+                onClick={this.onClick.bind(this)}
+                className={classnames(styles.item,
+                {
+                    [styles.itemHovered]: this.state.hovered
+                })}>
                 <h1 className={classnames(styles.title)}>{place.name}</h1>
-                <span>{place.rating / 5}</span>
+                <Rating className={styles.rating}
+                        percentage={(place.rating/5)}/>
             </div>
         );
     }
 }
+
+Item.propTypes = {
+    place: T.object.isRequired,
+    onHighlight: T.func
+};
+
+Item.defaultProps = {
+    onHighlight: () => {
+    },
+    offHighlight: () => {
+    }
+};
 
 export default Item;

480b2f89f8629b026e765c5adc73a36dd3692445
Aayush Kapoor <xeoneux@icloud.com>
2016-06-12 23:05:26 +0530

    Add markerson the map

--- src/views/Main/Map/Map.js
+++ src/views/Main/Map/Map.js
@@ -1,10 +1,22 @@
 import React, {PropTypes as T} from 'react'
 import classnames from 'classnames'
-import Map from 'google-maps-react'
+import Map, {Marker} from 'google-maps-react'
 
 import styles from './styles.module.css'
 
 export class MapComponent extends React.Component {
+    renderMarkers() {
+        if (!this.props.places) {
+            return null;
+        }
+        return this.props.places.map(place => {
+            return <Marker key={place.id}
+                           name={place.id}
+                           position={place.geometry.location}
+            />
+        });
+    }
+
     render() {
         return (
             <Map google={this.props.google}
